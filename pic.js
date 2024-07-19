62bef5f74d998dd0de3562f462e7cd7d4e140a9b
Nishantjaryal <nishantjaryal24@gmail.com>
2024-07-19 18:02:54 +0530

    adding capabilties of data fetching from the url

--- pic.js
+++ pic.js
@@ -3,8 +3,8 @@ const PicPicker = document.getElementById("PicPicker")
 const myfile = document.getElementById("file")
 const labels = document.querySelectorAll(".label")
 
-// const linkRunner = document.getElementById("link-runner")
-// const url = document.getElementById("url")
+const linkRunner = document.getElementById("link-runner")
+const url = document.getElementById("url")
 
 
 
@@ -21,32 +21,26 @@ Promise.all([
 
 
 
-// linkRunner.addEventListener("click", () => {
-//     PicPicker.style.display = "none"
-//     picContainer.innerHTML = ""
-//     picContainer.style.display = "block"
+linkRunner.addEventListener("click", () => {
+    PicPicker.style.display = "none"
+    picContainer.innerHTML = ""
+    picContainer.style.display = "block"
 
+    if (url.value !== "") {
+        const imgElementCode = `<img src="${url.value}" id="resultImage" class="resultImage" crossorigin="anonymous">`
+        picContainer.innerHTML = imgElementCode
+        const file = document.getElementById("resultImage")
+        runModel(file)
+    }
+    else {
 
-//     let imgElement = document.createElement('img');
+        const imgElement = document.createElement("img")
+        imgElement.src = "./public/undraw_Page_not_found_re_e9o6.png";
+        picContainer.appendChild(imgElement);
 
-//     if (url.value !== "") {
-//         const imgElement = document.createElement('img');
-//                     imgElement.classList.add("resultImage")
-//                     imgElement.src = url.value;
-//                     file = imgElement
-//                     console.log(file)
-//                     runModel(file)
-//                     picContainer.innerHTML = ""
-//                     picContainer.appendChild(imgElement);
-//     }
-//     else {
+    }
 
-//         imgElement.src = "./public/undraw_Page_not_found_re_e9o6.png";
-//         picContainer.appendChild(imgElement);
-
-//     }
-
-// })
+})
 
 
 labels.forEach((label) => {
@@ -124,33 +118,6 @@ function runModel(file) {
 
 
 
-// async function linkImageModel(file) {
-
-//     file.addEventListener('load', async () => {
-
-//         const canvas = await faceapi.createCanvasFromMedia(file)
-//         document.body.append(canvas)
-//         const displaySize = { width: file.width, height: file.height }
-//         faceapi.matchDimensions(canvas, displaySize)
-
-//         console.log("model running")
-
-//         setTimeout(async () => {     // pass element here to detect the face
-//             const detections = await faceapi.detectAllFaces(file,
-//                 new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-
-//             console.log(detections)
-
-//             const resizedDetections = faceapi.resizeResults(detections, displaySize)
-//             canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-//             faceapi.draw.drawDetections(canvas, resizedDetections)
-//             faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-//             faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)   // NOT WORK WELL WITH RESPONSIVENESS
-
-//             console.log("DONE!")
-//         }, 0);
-//     })
-// }
 
 
 
